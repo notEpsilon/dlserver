5c5523a45c53c6b8d5b01c92746ce649439feef7
notEpsilon <ibrahimnasr393@gmail.com>
2023-04-30 02:46:41 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { Redis } from "@upstash/redis";
+
+const app = express();
+
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ origin: process.env.FRONT_URL }));
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL || "",
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
+});
+
+app.get("/api/v1/problems/:owner", async (req: Request, res: Response) => {
+  const { owner } = req.params as { owner: string };
+  try {
+    const data = await redis.get(owner);
+    return res.status(200).json({ data, err: null });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ data: null, err });
+  }
+});
+
+app.post("/api/v1/problems/:owner", async (req: Request, res: Response) => {
+  const { owner } = req.params as { owner: string };
+  const { problems } = req.body as { problems: string };
+  try {
+    await redis.set(owner, problems);
+    return res.status(200).json({ data: "<OK>", err: null });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ data: null, err });
+  }
+});
