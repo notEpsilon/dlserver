8a0c6444434ba381d2f766fb6738d499fdc93e14
notEpsilon <ibrahimnasr393@gmail.com>
2023-04-30 04:42:47 +0300

    final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import cors from "cors";
-import { Redis } from "@upstash/redis";
+const express = require("express");
+const cors = require("cors");
+const { Redis } = require("@upstash/redis");
 
 const app = express();
 
